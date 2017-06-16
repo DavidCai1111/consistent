276ec8ba5144e0dbe6096ab8902143fedbaa4856
David Cai <davidcai1993@yahoo.com>
2017-06-16 17:23:50 +0800

    feat: add remove() method

--- src/consistant.rs
+++ src/consistant.rs
@@ -1,4 +1,6 @@
 use std::default::Default;
+use std::rc::Rc;
+use std::iter::Iterator;
 use chashmap::CHashMap;
 use crc::crc32::checksum_ieee;
 
@@ -6,9 +8,9 @@ use crc::crc32::checksum_ieee;
 pub struct Consistant {
     pub replicas_num: usize,
 
-    circle: CHashMap<u32, String>,
+    circle: CHashMap<u32, Rc<String>>,
     sorted_keys: Vec<u32>,
-    members: Vec<String>,
+    members: Vec<Rc<String>>,
 }
 
 impl Default for Consistant {
@@ -32,38 +34,67 @@ impl Consistant {
         }
     }
 
-    pub fn members(&self) -> &[String] {
-        &self.members
-    }
-
     pub fn count(&self) -> usize {
         self.members.len()
     }
 
-    pub fn add<S: Into<String>>(&mut self, element: S) {
-        let s = element.into();
-
-        for i in 0..self.replicas_num {
-            let sum = checksum_ieee(Self::generate_element_name(s.clone(), i).as_bytes());
-            self.circle.insert(sum, s.clone());
-
-            self.sorted_keys.push(sum);
+    pub fn add<S: Into<String>>(&mut self, element: S) -> Option<()> {
+        let s = Rc::new(element.into());
+        if self.contains(&s) {
+            return None;
         }
 
+        for i in 0..self.replicas_num {
+            let sum = checksum_ieee(Self::generate_element_name(&s, i).as_bytes());
+            self.circle.insert(sum, s.clone());
+            self.sorted_keys.push(sum)
+        }
+
+        self.members.push(s.clone());
         self.sorted_keys.sort();
-        self.members.push(s);
+        Some(())
     }
 
-    pub fn get<S: Into<String>>(&self, name: S) -> Option<String> {
-        if self.members.len() == 0 {
+    pub fn get<S: Into<String>>(&self, name: S) -> Option<Rc<String>> {
+        if self.circle.len() == 0 {
             return None;
         }
         let key = self.get_key(checksum_ieee(name.into().as_bytes()));
 
         match self.circle.get(&key) {
-            Some(v) => Some((*v).clone()),
+            Some(guard) => Some((*guard).clone()),
             None => unreachable!(),
         }
+    }
+
+    pub fn remove<S: Into<String>>(&mut self, name: S) -> Option<()> {
+        let s = Rc::new(name.into());
+        if !self.contains(&s) {
+            return None;
+        }
+
+        for i in 0..self.replicas_num {
+            let sum = checksum_ieee(Self::generate_element_name(&s, i).as_bytes());
+            self.circle.remove(&sum);
+
+            match self.sorted_keys.iter().position(|key| key.eq(&sum)) {
+                Some(index) => self.sorted_keys.remove(index),
+                None => unreachable!(),
+            };
+        }
+
+        match self.members.iter().position(|member| member.eq(&s)) {
+            Some(index) => self.members.remove(index),
+            None => unreachable!(),
+        };
+
+        Some(())
+    }
+
+    #[inline]
+    fn contains(&self, name: &str) -> bool {
+        let name = &Rc::new(name.into());
+        self.members.iter().find(|&e| e.eq(name)).is_some()
     }
 
     #[inline]
@@ -78,8 +109,8 @@ impl Consistant {
     }
 
     #[inline]
-    fn generate_element_name(element: String, i: usize) -> String {
-        element + &i.to_string()
+    fn generate_element_name(element: &str, i: usize) -> String {
+        String::from(element) + &i.to_string()
     }
 }
 
@@ -93,7 +124,6 @@ mod tests {
 
         assert_eq!(consistant.replicas_num, 20);
         assert_eq!(consistant.circle.len(), 0);
-        assert_eq!(consistant.members.len(), 0);
         assert_eq!(consistant.sorted_keys.len(), 0);
     }
 
@@ -103,16 +133,16 @@ mod tests {
 
         assert_eq!(consistant.replicas_num, 30);
         assert_eq!(consistant.circle.len(), 0);
-        assert_eq!(consistant.members.len(), 0);
         assert_eq!(consistant.sorted_keys.len(), 0);
     }
 
     #[test]
-    fn test_members() {
+    fn test_count() {
         let mut consistant = Consistant::default();
         consistant.add("cacheA");
         consistant.add("cacheB");
-        assert_eq!(consistant.members.len(), 2);
+        consistant.add("cacheC");
+        assert_eq!(consistant.count(), 3);
     }
 
     #[test]
@@ -124,6 +154,16 @@ mod tests {
 
         assert_eq!(consistant.circle.len(), 3 * consistant.replicas_num);
         assert_eq!(consistant.sorted_keys.len(), 3 * consistant.replicas_num);
+    }
+
+    #[test]
+    fn test_contains() {
+        let mut consistant = Consistant::default();
+        consistant.add("cacheA");
+
+        assert_eq!(consistant.contains("cacheA"), true);
+        assert_eq!(consistant.contains("cacheB"), false);
+        assert_eq!(consistant.contains("CachEa"), false);
     }
 
     #[test]
@@ -139,6 +179,30 @@ mod tests {
                    consistant.get("kally").unwrap());
         assert_eq!(consistant.get("jason").unwrap(),
                    consistant.get("jason").unwrap());
+    }
+
+    #[test]
+    fn test_remove() {
+        let mut consistant = Consistant::default();
+        consistant.add("cacheA");
+        consistant.add("cacheB");
+        consistant.add("cacheC");
+
+        consistant.remove("cacheC");
+        assert_eq!(consistant.count(), 2);
+
+        assert!(!consistant
+                    .get("david")
+                    .unwrap()
+                    .eq(&Rc::new(String::from("cacheC"))));
+        assert!(!consistant
+                    .get("kally")
+                    .unwrap()
+                    .eq(&Rc::new(String::from("cacheC"))));
+        assert!(!consistant
+                    .get("jason")
+                    .unwrap()
+                    .eq(&Rc::new(String::from("cacheC"))));
     }
 }
 
